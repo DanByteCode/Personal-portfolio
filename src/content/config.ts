092ec25869a9220c72df48c99523d43f8b0cea29
Darknhyt <40185738+Darknhyt@users.noreply.github.com>
2024-12-17 13:11:28 -0400

    Adding Responsive

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,17 @@
 import { defineCollection, z } from 'astro:content'
 
-const projects = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    preview: z.string(),
-    img: z.string(),
-    live: z.string(),
-    code: z.string(),
-    tecnology: z.array( z.string() ),
-  })
+const projectSchema = z.object({
+  title: z.string(),
+  preview: z.string(),
+  img: z.string(),
+  live: z.string(),
+  code: z.string(),
+  tecnology: z.array(z.string())
 })
 
+const projects = defineCollection({
+  type: 'content',
+  schema: projectSchema
+})
 export const collections = { projects }
+export type Project = { data: z.infer<typeof projectSchema>; body: string }
